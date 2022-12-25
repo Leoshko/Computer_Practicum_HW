3e07f737277e81e788f660cc17a9a6cb51ef435c
Veronika Leoshko <veronica.leoshko@gmail.com>
2022-12-25 17:36:21 +0100

    Almost make 1st task

--- main.c
+++ main.c
@@ -5,39 +5,45 @@ void main() {
     int G = 0;  // G - ground floor
     int floor;
 
+    printf("The Elevator travels between five floors.\n");
+    printf("\n");
+    printf("To go the Ground floor press 0. \nTo go to the 1st floor press 1. \nTo go to the 2nd floor press 2. \nTo go to the 3rd floor press 3. \nTo go to the 4th floor press 4. \nTo go to the 5th floor press 5. \n");
+    printf("\n");
     printf("You're now on the Ground floor. What floor do you want to go to? \n");
-    scanf("%i", &floor);
 
+    while (1) {
+        scanf("%i", &floor);
+            switch(floor) {
+                case 0:
+                printf("You're now on the Ground floor. What floor do you want to go next? \n");
+                break;
+
+                case 1:
+                printf("You're now on the 1st floor. What floor do you want to go next? \n");
+                break;
+
+                case 2:
+                printf("You're now on the 2nd floor. What floor do you want to go next? \n");
+                break;
+
+                case 3:
+                printf("You're now on the 3d floor. What floor do you want to go next? \n");
+                break;
+
+                case 4:
+                printf("You're now on the 4th floor. What floor do you want to go next? \n");
+                break;
+
+                case 5:
+                printf("You're now on the 5th floor. What floor do you want to go next? \n");
+                break;
+
+                default:
+                printf("Please, enter a vaild value: \n");
+                break;
+                
+            }
     
-    if (floor == G) {
-        printf("Lift is now on the Ground floor");
-    }
-    else if (G < floor && floor <= 5) {
-        switch(floor) {
-
-        case 1:
-        printf("Lift is now on the 1st floor");
-        break;
-
-        case 2:
-        printf("Lift is now on the 2nd floor");
-        break;
-
-        case 3:
-        printf("Lift is now on the 3d floor");
-        break;
-
-        case 4:
-        printf("Lift is now on the 4th floor");
-        break;
-
-        case 5:
-        printf("Lift is now on the 5th floor");
-        break;
-        }
-    } 
-    else {
-        printf("Please, enter a vaild value");
     }
     
 
